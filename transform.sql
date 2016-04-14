d249dc6533e3fe8e4fc226ccd041f1e9910ec615
rohan-thakur <rohandthakur@gmail.com>
2016-04-13 22:32:05 -0400

    Created table to compare traffic from twitter vs blockchain

---
+++ transform.sql
@@ -0,0 +1,10 @@
+CREATE TABLE compare_traffic AS 
+(SELECT DATE(a.timestamp) as dt, 
+       date_part('hour', a.timestamp) as hour, 
+       COUNT(DISTINCT a.transaction_id) as num_transactions, 
+       COUNT(DISTINCT b.tweet) as num_tweets
+FROM transactioncount a
+     INNER JOIN
+     tweetcount b
+     ON date_part('hour', a.timestamp) = date_part('hour', b.timestamp)
+GROUP BY 1,2);
