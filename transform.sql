fd5c366afa40a23554121eba59a4a4856a91d44d
rohan-thakur <rohandthakur@gmail.com>
2016-04-16 15:49:48 -0400

    optimized transform

--- transform.sql
+++ transform.sql
@@ -1,10 +1,16 @@
 CREATE TABLE compare_traffic AS 
-(SELECT DATE(a.timestamp) as dt, 
-       date_part('hour', a.timestamp) as hour, 
+(SELECT a.dt as dt, 
+       a.hour as hour, 
        COUNT(DISTINCT a.transaction_id) as num_transactions, 
        COUNT(DISTINCT b.tweet) as num_tweets
-FROM transactioncount a
-     INNER JOIN
-     tweetcount b
-     ON date_part('hour', a.timestamp) = date_part('hour', b.timestamp)
+FROM (SELECT DATE(timestamp) as dt,
+             date_part('hour', timestamp) as hour,
+             transaction_id
+      FROM transactioncount) a
+     LEFT JOIN
+     (SELECT DATE(timestamp) as dt,
+             date_part('hour',timestamp) as hour,
+             tweet
+      FROM tweetcount) b
+     ON a.dt = b.dt AND a.hour = b.hour
 GROUP BY 1,2);
